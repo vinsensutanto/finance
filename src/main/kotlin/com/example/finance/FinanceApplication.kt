651904237475acff0f5406d68f84409e92ec7e74
vinsensutanto <vinsensutanto@gmail.com>
2025-09-20 20:01:04 +0700

    initial

---
+++ src/main/kotlin/com/example/finance/FinanceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.finance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceApplication>(*args)
+}
